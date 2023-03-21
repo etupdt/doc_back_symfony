6b56f8bdceb4d4e1f3a565a9a4470066fea2f2d5
Denis Tavernier <denis-tavernier@wanadoo.fr>
2023-03-21 20:10:11 +0100

    add appspec for aws 5

--- scripts/restartapache.sh
+++ scripts/restartapache.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo 'exec restartapache.sh'
 
+cat /etc/apache2/sites-available/000-default.conf | grep -v "9443.conf" > /etc/apache2/sites-available/000-default.conf
+echo "Include /var/www/html/doc-back-symfony/scripts/000-default-9443.conf" >> /etc/apache2/sites-available/000-default.conf
+
 sudo service apache2 restart
 #sudo systemctl restart httpd
