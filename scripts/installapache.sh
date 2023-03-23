f1d6c86313ca12c12c70fc24598e3cce3c940c1b
Denis Tavernier <denis-tavernier@wanadoo.fr>
2023-03-23 14:09:16 +0100

    add install database migration 18

--- scripts/installapache.sh
+++ scripts/installapache.sh
@@ -10,13 +10,6 @@ sudo apt install libapache2-mod-php
 cat /etc/apache2/sites-available/000-default.conf | grep -v "9443.conf" | sudo tee /etc/apache2/sites-available/000-default.conf > /dev/null
 echo "Include /var/www/html/doc-back-symfony/scripts/000-default-9443.conf" | sudo tee -a /etc/apache2/sites-available/000-default.conf > /dev/null
 
-sudo apt install composer
-
-wget https://get.symfony.com/cli/installer -O - | bash
-export APP_ENV=prod 
-export APP_DEBUG=0 
-composer install --no-dev --optimize-autoloader | sudo tee /tmp/log > /dev/null
-
 php /var/www/html/doc-back-symfony/bin/console doctrine:migration:migrate
 
 sudo service apache2 start
