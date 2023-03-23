f61b2d31bbfa402e40a467cf65d288135181d11a
Denis Tavernier <denis-tavernier@wanadoo.fr>
2023-03-23 08:17:07 +0100

    add install database migration

--- scripts/installapache.sh
+++ scripts/installapache.sh
@@ -3,7 +3,12 @@ echo 'exec installapache.sh'
 
 sudo apt update
 sudo apt install apache2
+
+##### sudo apt install php8.1-cli
+sudo apt install libapache2-mod-php
+
 sudo service apache2 start
+
 #sudo yum update -y
 #sudo yum install -y httpd
 #sudo systemctl start httpd
