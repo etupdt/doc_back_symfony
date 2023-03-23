93f4c3a88c22ae6c8200cf6b11079dc73ff64238
Denis Tavernier <denis-tavernier@wanadoo.fr>
2023-03-23 11:17:01 +0100

    add install database migration 12

--- scripts/restartapache.sh
+++ scripts/restartapache.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 echo 'exec restartapache.sh'
 
-sudo wget https://get.symfony.com/cli/installer -O - | bash
-export APP_ENV=prod 
-export APP_DEBUG=0 
-sudo composer install --no-dev --optimize-autoloader
-
 php /var/www/html/doc-back-symfony/bin/console doctrine:migration:migrate
 
 cat /etc/apache2/sites-available/000-default.conf | grep -v "9443.conf" | sudo tee /etc/apache2/sites-available/000-default.conf > /dev/null
