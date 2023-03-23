bf37761ad6202bd45bb39a97c3d5c3c89ac33952
Denis Tavernier <denis-tavernier@wanadoo.fr>
2023-03-23 12:00:35 +0100

    add install database migration 14

--- scripts/installapache.sh
+++ scripts/installapache.sh
@@ -7,6 +7,18 @@ sudo apt install apache2
 ##### sudo apt install php8.1-cli
 sudo apt install libapache2-mod-php
 
+cat /etc/apache2/sites-available/000-default.conf | grep -v "9443.conf" | sudo tee /etc/apache2/sites-available/000-default.conf > /dev/null
+echo "Include /var/www/html/doc-back-symfony/scripts/000-default-9443.conf" | sudo tee -a /etc/apache2/sites-available/000-default.conf > /dev/null
+
+sudo apt install composer
+
+wget https://get.symfony.com/cli/installer -O - | bash
+export APP_ENV=prod 
+export APP_DEBUG=0 
+composer install --no-dev --optimize-autoloader
+
+php /var/www/html/doc-back-symfony/bin/console doctrine:migration:migrate
+
 sudo service apache2 start
 
 #sudo yum update -y
--- scripts/restartapache.sh
+++ scripts/restartapache.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 echo 'exec restartapache.sh'
 
-php /var/www/html/doc-back-symfony/bin/console doctrine:migration:migrate
-
-cat /etc/apache2/sites-available/000-default.conf | grep -v "9443.conf" | sudo tee /etc/apache2/sites-available/000-default.conf > /dev/null
-echo "Include /var/www/html/doc-back-symfony/scripts/000-default-9443.conf" | sudo tee -a /etc/apache2/sites-available/000-default.conf > /dev/null
-
 sudo service apache2 restart
 #sudo systemctl restart httpd
