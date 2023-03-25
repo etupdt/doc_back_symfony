f9b8f65b8a34e342db338bba0c0b58dbcb57a640
Denis Tavernier <denis-tavernier@wanadoo.fr>
2023-03-25 14:20:27 +0100

    add composer.phar path

--- scripts/installapache.sh
+++ scripts/installapache.sh
@@ -10,7 +10,7 @@ sudo apt install libapache2-mod-php
 cat /etc/apache2/sites-available/000-default.conf | grep -v "9443.conf" | sudo tee /etc/apache2/sites-available/000-default.conf > /dev/null
 echo "Include /var/www/html/doc-back-symfony/scripts/000-default-9443.conf" | sudo tee -a /etc/apache2/sites-available/000-default.conf > /dev/null
 
-./composer.phar install --no-dev --optimize-autoloader
+/var/www/html/doc-back-symfony/composer.phar install --no-dev --optimize-autoloader
 
 php /var/www/html/doc-back-symfony/bin/console doctrine:migration:migrate
 
