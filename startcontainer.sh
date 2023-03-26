d511dbc7413db33a41bcd176463093fa53c367c6
Denis Tavernier <denis-tavernier@wanadoo.fr>
2023-03-26 13:57:07 +0200

    add apache config path

--- startcontainer.sh
+++ startcontainer.sh
@@ -2,14 +2,14 @@
 #!/bin/bash
 echo 'exec installapache.sh'
 
-sudo apt update
-sudo apt install apache2
+apt update
+apt install apache2
 
 ##### sudo apt install php8.1-cli
-sudo apt install libapache2-mod-php
+apt install libapache2-mod-php
 
-cat /etc/apache2/sites-available/000-default.conf | grep -v "9443.conf" | sudo tee /etc/apache2/sites-available/000-default.conf > /dev/null
-echo "Include ./scripts/000-default-9443.conf" | sudo tee -a /etc/apache2/sites-available/000-default.conf > /dev/null
+cat /etc/apache2/sites-available/000-default.conf | grep -v "9443.conf" | tee /etc/apache2/sites-available/000-default.conf > /dev/null
+echo "Include /var/www/html/doc_back_symfony/scripts/000-default-9443.conf" | tee -a /etc/apache2/sites-available/000-default.conf > /dev/null
 
 export APP_ENV=prod 
 export APP_DEBUG=0 
@@ -19,13 +19,13 @@ export APP_DEBUG=0
 
 php bin/console doctrine:migration:migrate
 
-sudo a2enmod rewrite
-sudo a2enmod ssl 
+a2enmod rewrite
+a2enmod ssl 
 
-sudo service apache2 start
+service apache2 start
 
 #sudo yum update -y
 #sudo yum install -y httpd
 #sudo systemctl start httpd
 
-#tail -f /dev/null
+tail -f /dev/null
