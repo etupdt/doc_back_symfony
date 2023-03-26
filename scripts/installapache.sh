d1e0b3c8fe55d02bf7e96cb46da0487b0d4107d0
Denis Tavernier <denis-tavernier@wanadoo.fr>
2023-03-26 13:39:34 +0200

    add apache config

--- scripts/installapache.sh
+++ scripts/installapache.sh
@@ -8,15 +8,18 @@ sudo apt install apache2
 sudo apt install libapache2-mod-php
 
 cat /etc/apache2/sites-available/000-default.conf | grep -v "9443.conf" | sudo tee /etc/apache2/sites-available/000-default.conf > /dev/null
-echo "Include /var/www/html/doc-back-symfony/scripts/000-default-9443.conf" | sudo tee -a /etc/apache2/sites-available/000-default.conf > /dev/null
+echo "Include ./scripts/000-default-9443.conf" | sudo tee -a /etc/apache2/sites-available/000-default.conf > /dev/null
 
 export APP_ENV=prod 
 export APP_DEBUG=0 
 
-sudo chmod 755 /var/www/html/doc-back-symfony/composer.phar
-/var/www/html/doc-back-symfony/composer.phar install --no-dev --optimize-autoloader
+#sudo chmod 755 /var/www/html/doc-back-symfony/composer.phar
+./composer.phar install --no-dev --optimize-autoloader
 
-php /var/www/html/doc-back-symfony/bin/console doctrine:migration:migrate
+php bin/console doctrine:migration:migrate
+
+sudo a2enmod rewrite
+sudo a2enmod ssl 
 
 sudo service apache2 start
 
