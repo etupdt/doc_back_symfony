38f6b572eb0e77a64978c837717e7f9fe3ccf1a9
Denis Tavernier <denis-tavernier@wanadoo.fr>
2023-04-05 20:48:29 +0200

    modify sequence cdk

--- scripts/restartapache.sh
+++ scripts/restartapache.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo 'exec restartapache.sh'
 
-sudo service apache2 restart
+docker exec -it my_container sh -c "cd /var/www/html/doc_back_symfony && php bin/console doctrine:migration:migrate && apt install pwgen -y"
+
 #sudo systemctl restart httpd
--- scripts/startapache.sh
+++ scripts/startapache.sh
@@ -1,5 +1,1 @@
 #!/bin/bash
-echo 'exec startapache.sh'
-
-sudo service apache2 start
-#sudo systemctl start httpd
