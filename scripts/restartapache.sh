17a91d0434e40c4dfca6fe98b08e666b3082ec73
Denis Tavernier <denis-tavernier@wanadoo.fr>
2023-04-06 11:48:20 +0200

    change appspec : add call to startconatiener 2

--- scripts/restartapache.sh
+++ scripts/restartapache.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker exec -it my_container sh -c "cd /var/www/html/doc_back_symfony && ./scripts/"
+docker exec -it my_container sh -c "cd /var/www/html/doc_back_symfony && ./scripts/startcontainer.sh"
 
 #sudo systemctl restart httpd
