b74b4cdb966540c98ad18931285bc80155e83b64
Denis Tavernier <denis-tavernier@wanadoo.fr>
2023-04-10 18:37:06 +0200

    move init to cloudformation 9

--- scripts/restartapache-aws.sh
+++ scripts/restartapache-aws.sh
@@ -30,17 +30,17 @@ EOS
 #=============================================== symfony =====================================================
 echo 'symfony' | sudo tee -a $log
 
-pwd | sudo tee -a /var/log/deploy/restartapache.log
+pwd | sudo tee -a $log
 
-curl -sS https://getcomposer.org/installer | sudo php | sudo tee -a /var/log/deploy/restartapache.log
+curl -sS https://getcomposer.org/installer | sudo php |& sudo tee -a $log
 
-sudo mv -f composer.phar /usr/local/bin/composer | sudo tee -a /var/log/deploy/restartapache.log
-sudo ln -sf /usr/local/bin/composer /usr/bin/composer | sudo tee -a /var/log/deploy/restartapache.log
+sudo mv -f composer.phar /usr/local/bin/composer |& sudo tee -a $log
+sudo ln -sf /usr/local/bin/composer /usr/bin/composer |& sudo tee -a $log
 
-sudo composer install --no-dev --optimize-autoloader | sudo tee -a /var/log/deploy/restartapache.log
+sudo composer install --no-dev --optimize-autoloader |& sudo tee -a $log
 
-sudo php bin/console doctrine:database:create | sudo tee -a /var/log/deploy/restartapache.log
-sudo php bin/console doctrine:migration:migrate --quiet | sudo tee -a /var/log/deploy/restartapache.log
+sudo php bin/console doctrine:database:create |& sudo tee -a $log
+sudo php bin/console doctrine:migration:migrate --quiet |& sudo tee -a $log
 
 #export APP_ENV=prod 
 #export APP_DEBUG=0 
@@ -52,12 +52,12 @@ passphrase=$(tr -dc A-Za-z0-9 < /dev/urandom | head -c 32 | xargs)
 
 sudo sed -i "s/^JWT_PASSPHRASE=.*$/JWT_PASSPHRASE=$passphrase/g" '.env'
 
-sudo mkdir -p config/jwt/ | sudo tee -a /var/log/deploy/restartapache.log
+sudo mkdir -p config/jwt/ | sudo tee -a $log
 
-echo "creation de la clé privée" | sudo tee -a /var/log/deploy/restartapache.log
+echo "creation de la clé privée" | sudo tee -a $log
 sudo openssl genpkey -out config/jwt/private.pem -aes256 -algorithm rsa -pkeyopt rsa_keygen_bits:4096 -pass pass:$passphrase
 
-echo "creation de la clé public" | sudo tee -a /var/log/deploy/restartapache.log
+echo "creation de la clé public" | sudo tee -a $log
 sudo openssl pkey -in config/jwt/private.pem -out config/jwt/public.pem -pubout -passin pass:$passphrase
 
 #echo "change mod tout le site" | sudo tee -a /var/log/deploy/restartapache.log
@@ -79,4 +79,4 @@ echo "Include /var/www/html/doc_back_symfony/scripts/httpd-vhosts-9443.conf" | s
 cat /etc/httpd/conf/httpd.conf | grep -v "Listen 9443" | sudo tee /etc/httpd/conf/httpd.conf > /dev/null
 echo "Listen 9443" | sudo tee -a /etc/httpd/conf/httpd.conf > /dev/null
 
-sudo service httpd start
+sudo service httpd start |& tee -a $log
